a920515737d389c87dcebeb652f75c53d5b45e4c
Michael Dialogo <m.dialogo@gmail.com>
2013-04-07 15:01:31 +0800

    initial

---
+++ SmartTextBox/Form1.Designer.cs
@@ -0,0 +1,114 @@
+namespace SmartTextBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userControlTextBox3 = new DataLift.userControlTextBox();
+            this.userControlTextBox2 = new DataLift.userControlTextBox();
+            this.userControlTextBox1 = new DataLift.userControlTextBox();
+            this.SuspendLayout();
+            // 
+            // userControlTextBox3
+            // 
+            this.userControlTextBox3.BorderColor = System.Drawing.Color.LightGray;
+            this.userControlTextBox3.BorderSize = 1;
+            this.userControlTextBox3.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.userControlTextBox3.DisplayFocusBorder = true;
+            this.userControlTextBox3.FocusBorderColor = System.Drawing.Color.Blue;
+            this.userControlTextBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userControlTextBox3.Location = new System.Drawing.Point(13, 43);
+            this.userControlTextBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.userControlTextBox3.MaxLength = 1;
+            this.userControlTextBox3.Name = "userControlTextBox3";
+            this.userControlTextBox3.Size = new System.Drawing.Size(39, 22);
+            this.userControlTextBox3.TabIndex = 2;
+            this.userControlTextBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.userControlTextBox3.ValidationCustomChars = "1234567";
+            this.userControlTextBox3.ValidationType = DataLift.ValidationType.CustomChars;
+            this.userControlTextBox3.WordSpill = false;
+            // 
+            // userControlTextBox2
+            // 
+            this.userControlTextBox2.BorderColor = System.Drawing.Color.LightGray;
+            this.userControlTextBox2.BorderSize = 1;
+            this.userControlTextBox2.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.userControlTextBox2.DisplayFocusBorder = true;
+            this.userControlTextBox2.FocusBorderColor = System.Drawing.Color.Blue;
+            this.userControlTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userControlTextBox2.Location = new System.Drawing.Point(171, 13);
+            this.userControlTextBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.userControlTextBox2.MaxLength = 32767;
+            this.userControlTextBox2.Name = "userControlTextBox2";
+            this.userControlTextBox2.Size = new System.Drawing.Size(100, 22);
+            this.userControlTextBox2.TabIndex = 1;
+            this.userControlTextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.userControlTextBox2.ValidationCustomChars = "";
+            this.userControlTextBox2.ValidationType = DataLift.ValidationType.AlphaNumericOnly;
+            this.userControlTextBox2.WordSpill = false;
+            // 
+            // userControlTextBox1
+            // 
+            this.userControlTextBox1.BorderColor = System.Drawing.Color.LightGray;
+            this.userControlTextBox1.BorderSize = 1;
+            this.userControlTextBox1.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.userControlTextBox1.DisplayFocusBorder = true;
+            this.userControlTextBox1.FocusBorderColor = System.Drawing.Color.Blue;
+            this.userControlTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userControlTextBox1.Location = new System.Drawing.Point(13, 13);
+            this.userControlTextBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.userControlTextBox1.MaxLength = 10;
+            this.userControlTextBox1.Name = "userControlTextBox1";
+            this.userControlTextBox1.Size = new System.Drawing.Size(150, 22);
+            this.userControlTextBox1.TabIndex = 0;
+            this.userControlTextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.userControlTextBox1.ValidationCustomChars = "";
+            this.userControlTextBox1.ValidationType = DataLift.ValidationType.AlphaNumericOnly;
+            this.userControlTextBox1.WordSpill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.userControlTextBox3);
+            this.Controls.Add(this.userControlTextBox2);
+            this.Controls.Add(this.userControlTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DataLift.userControlTextBox userControlTextBox1;
+        private DataLift.userControlTextBox userControlTextBox2;
+        private DataLift.userControlTextBox userControlTextBox3;
+    }
+}
+
---
+++ SmartTextBox/userControlTextBox.cs
@@ -0,0 +1,300 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DataLift
+{
+    public enum ValidationType
+    {
+        None,
+        NumericOnly,
+        AlphaOnly,
+        AlphaNumericOnly,
+        AmountOnly,
+        CustomChars
+    }
+
+    public partial class userControlTextBox : UserControl
+    {
+        private TextBox textBox;
+        private Color borderColor = Color.LightGray;
+        private Color origBordercolor;
+        private int borderSize = 1;
+        private Color focusBorderColor = Color.Blue;
+        private bool displayFocusBorder = true;
+        private int numErrors = 0;
+        private ValidationType validationType = ValidationType.AlphaNumericOnly;
+        private String validationCustChars = "";
+
+        // constructor
+        public userControlTextBox()
+        {
+            InitializeComponent();
+
+            textBox = new TextBox();
+            this.textBox.BorderStyle = BorderStyle.None;
+            this.textBox.CharacterCasing = CharacterCasing.Upper;
+            this.textBox.AutoSize = false;
+            this.textBox.KeyPress += new KeyPressEventHandler(PerformDataValidation);
+            this.textBox.KeyPress += new KeyPressEventHandler(textBox_KeyPress);
+
+            this.Controls.Add(textBox);
+            InvalidateSize();
+        }
+
+        private void textBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Control currentControl = ((Control)sender).Parent;
+            if (wordSpill && currentControl.Text.Length >= this.MaxLength)
+            {
+                string lastWord = currentControl.Text.Substring(currentControl.Text.LastIndexOf(" ") + 1);
+                currentControl.Text = currentControl.Text.Substring(0, currentControl.Text.LastIndexOf(" "));
+                Control nextControl = currentControl.Parent.GetNextControl(currentControl, true);
+                nextControl.Text = lastWord + e.KeyChar;
+                nextControl.Focus();
+                SendKeys.SendWait("{RIGHT}");
+                e.Handled = true;
+            }
+        }
+
+        void PerformDataValidation(object sender, KeyPressEventArgs e)
+        {
+            switch (validationType)
+            {
+                case ValidationType.None:
+                    break;
+                case ValidationType.NumericOnly:
+                    e.Handled = (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar));
+                    break;
+                case ValidationType.AlphaOnly:
+                    e.Handled = (!Char.IsLetter(e.KeyChar));
+                    break;
+                case ValidationType.AlphaNumericOnly:
+                    break;
+                case ValidationType.CustomChars:
+                    e.Handled = (!Char.IsControl(e.KeyChar) && !validationCustChars.ToUpper().Contains(e.KeyChar.ToString().ToUpper()));
+                    break;
+            }
+            if (e.Handled)
+                numErrors++;
+            else
+                numErrors = 0;
+
+            if (numErrors >= 3)
+                this.toolTip1.Show("Enter " + ValidationTypeExtension.ToString(validationType, validationCustChars), this.textBox);
+            else
+                this.toolTip1.Hide(this.textBox);
+        }
+
+        // properties
+        public TextBox InnerTextBox
+        {
+            get { return textBox; }
+        }
+        public Color BorderColor
+        {
+            get { return borderColor; }
+            set
+            {
+                borderColor = value;
+                Invalidate();
+            }
+        }
+        public int BorderSize
+        {
+            get { return borderSize; }
+            set
+            {
+                borderSize = value;
+                Invalidate();
+            }
+        }
+        public override Color BackColor
+        {
+            get
+            {
+                return this.textBox.BackColor;
+            }
+            set
+            {
+                this.textBox.BackColor = value;
+            }
+        }
+        public Color FocusBorderColor
+        {
+            get { return focusBorderColor; }
+            set { focusBorderColor = value; }
+        }
+        public bool DisplayFocusBorder
+        {
+            get { return displayFocusBorder; }
+            set { displayFocusBorder = value; }
+        }
+        public override string Text
+        {
+            get
+            {
+                return this.textBox.Text;
+            }
+            set
+            {
+                this.textBox.Text = value;
+            }
+        }
+        public int MaxLength
+        {
+            get { return this.textBox.MaxLength; }
+            set { this.textBox.MaxLength = value; }
+        }
+        public HorizontalAlignment TextAlign
+        {
+            get { return this.textBox.TextAlign; }
+            set { this.textBox.TextAlign = value; }
+        }
+        public ValidationType ValidationType
+        {
+            get { return validationType; }
+            set { validationType = value; }
+        }
+        public String ValidationCustomChars
+        {
+            get { return validationCustChars; }
+            set { validationCustChars = value; }
+        }
+        public CharacterCasing CharacterCasing
+        {
+            get { return textBox.CharacterCasing; }
+            set { textBox.CharacterCasing = value; }
+        }
+        private bool wordSpill = false;
+        public bool WordSpill
+        {
+            get { return wordSpill; }
+            set { wordSpill = value; }
+        }
+
+        // events
+        private void userControlTextBox_Paint(object sender, PaintEventArgs e)
+        {
+            ControlPaint.DrawBorder(e.Graphics, this.ClientRectangle
+                , borderColor, borderSize, ButtonBorderStyle.Solid
+                , borderColor, borderSize, ButtonBorderStyle.Solid
+                , borderColor, borderSize, ButtonBorderStyle.Solid
+                , borderColor, borderSize, ButtonBorderStyle.Solid);
+        }
+        private void userControlTextBox_Resize(object sender, EventArgs e)
+        {
+            InvalidateSize();
+        }
+        private void InvalidateSize()
+        {
+            textBox.Size = new Size(this.Width - 4, this.Height - 4);
+            textBox.Location = new Point(2, 2);
+        }
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.Enter)
+                SendKeys.SendWait("{TAB}");
+            else if (keyData == (Keys.Shift | Keys.Enter))
+                SendKeys.SendWait("+{TAB}");
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        public event KeyEventHandler TextBoxKeyUp
+        {
+            add
+            {
+                this.textBox.KeyUp += value;
+            }
+            remove
+            {
+                this.textBox.KeyUp -= value;
+            }
+        }
+        public event KeyEventHandler TextBoxKeyDown
+        {
+            add
+            {
+                this.textBox.KeyDown += value;
+            }
+            remove
+            {
+                this.textBox.KeyDown -= value;
+            }
+        }
+        public event KeyPressEventHandler TextBoxKeyPress
+        {
+            add
+            {
+                this.textBox.KeyPress += value;
+            }
+            remove
+            {
+                this.textBox.KeyPress -= value;
+            }
+        }
+
+        protected override void OnEnter(EventArgs e)
+        {
+            this.textBox.SelectionStart = 0;
+            this.textBox.SelectionLength = textBox.TextLength;
+
+            if (DisplayFocusBorder)
+            {
+                //save orig color;
+                origBordercolor = borderColor;
+                borderSize = 2;
+                borderColor = focusBorderColor;
+                Invalidate();
+            }
+            base.OnEnter(e);
+        }
+        protected override void OnLeave(EventArgs e)
+        {
+            if (DisplayFocusBorder)
+            {
+                borderSize = 1;
+                borderColor = origBordercolor;
+                Invalidate();
+            }
+            this.toolTip1.Hide(this.textBox);
+            base.OnLeave(e);
+        }
+    }
+
+    public static class ValidationTypeExtension
+    {
+        public static string ToString(ValidationType validationType, string customChars)
+        {
+            string result = "";
+            switch (validationType)
+            {
+                case ValidationType.None:
+                    result = "None";
+                    break;
+                case ValidationType.NumericOnly:
+                    result = "Numeric Only";
+                    break;
+                case ValidationType.AlphaOnly:
+                    result = "Alpha Only";
+                    break;
+                case ValidationType.AlphaNumericOnly:
+                    result = "Alpha-numeric Only";
+                    break;
+                case ValidationType.AmountOnly:
+                    result = "Amout Only";
+                    break;
+                case ValidationType.CustomChars:
+                    foreach (char c in customChars)
+                        result += (customChars.EndsWith(c.ToString()) ? "or " + c.ToString() : c.ToString() + ", ");
+                    result += " Only";
+                    break;
+            }
+            return result;
+        }
+    }
+}
